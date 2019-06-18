b30de5cecd3283265b42b789e09dc040cc160ea4
ChefyHase <ChefyHase@example.com>
2019-06-18 16:58:22 +0900

    update

--- model.js
+++ model.js
@@ -13,11 +13,13 @@ class Model {
 
     const conv1d = tf.layers.conv2d({ dataFormat: 'channelsFirst', filters: 20, kernelSize: [1, 1], strides: 1 }).apply(input);
     const encoderActiv1 = tf.layers.leakyReLU().apply(conv1d);
-    const conv2d = tf.layers.conv2d({ dataFormat: 'channelsFirst', filters: 5, kernelSize: [1, 1], strides: 1 }).apply(encoderActiv1);
+    const conv2d = tf.layers.conv2d({ dataFormat: 'channelsFirst', filters: 10, kernelSize: [1, 1], strides: 1 }).apply(encoderActiv1);
     const encoderActiv2 = tf.layers.leakyReLU().apply(conv2d);
+    const conv3d = tf.layers.conv2d({ dataFormat: 'channelsFirst', filters: 5, kernelSize: [1, 1], strides: 1 }).apply(encoderActiv2);
+    const encoderActiv3 = tf.layers.leakyReLU().apply(conv3d);
 
-    const flatten = tf.layers.flatten().apply(encoderActiv2);
-    const dense = tf.layers.dense({ units: 512 }).apply(flatten);
+    const flatten = tf.layers.flatten().apply(encoderActiv3);
+    const dense = tf.layers.dense({ units: 1024 }).apply(flatten);
     const decoderActiv = tf.layers.leakyReLU().apply(dense);
     const drop1 = tf.layers.dropout({ rate: 0.5 }).apply(decoderActiv);
 
